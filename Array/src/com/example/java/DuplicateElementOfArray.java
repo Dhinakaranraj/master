9ea22ae3d791d7d3bb96bc0bd0c970df51213f47
Dhinakaranraj <dhinakarntm@gmail.com>
2024-04-07 21:03:23 +0530

    just push

---
+++ Array/src/com/example/java/DuplicateElementOfArray.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+public class DuplicateElementOfArray {
+	public static void main(String args[]) {
+		
+		int[]arr= {1,6,2,6,1,};
+		
+		System.out.print("Array Elements:");
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		System.out.print("\nDuplicate Array: ");
+		for(int i=0; i<arr.length; i++) {
+			
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i]==arr[j]) {
+					System.out.print(arr[j]+" ");
+				}
+			}
+		}
+		
+		
+	}
+
+}
