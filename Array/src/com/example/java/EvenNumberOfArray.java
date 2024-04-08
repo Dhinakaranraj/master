39dd7525d4066cce6382f8c20d7ac53f21131d52
Dhinakaranraj <dhinakarntm@gmail.com>
2024-04-08 07:54:16 +0530

    name update

--- Array/src/com/example/java/EvenNumberOfArray.java
+++ Array/src/com/example/java/EvenNumberOfArray.java
@@ -2,7 +2,7 @@ package com.example.java;
 
 //10.Program to print the elements of an array present on even position
 
-public class EvenPositionOfArray {
+public class EvenNumberOfArray {
        public static void main(String args[]) {
     	   
     	   int[] arr= {1,2,3,4,5,6};
