2dfcb8edd60ea2992d3be043e0df4d8bacd33c46
Dhinakaranraj <dhinakarntm@gmail.com>
2024-04-08 07:24:29 +0530

    push

---
+++ Array/src/com/example/java/PrintArray.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+//8.Program to print the elements of an array
+
+public class PrintArray {
+      public static void main(String args[]) {
+    	  
+    	  int[] arr={10,90,57,34,60,100};
+    	  
+    	  System.out.print("Elements Of Array: ");
+    	  for(int i=0; i<arr.length; i++) {
+    		  System.out.print(arr[i]+" ");
+    	  }
+      }
+}
