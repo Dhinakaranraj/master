d8df169ba038f25e459665780cae046d784c3e86
Dhinakaranraj <dhinakarntm@gmail.com>
2024-04-24 20:00:43 +0530

    start with method

---
+++ String/src/com/example/string/StartWithFun.java
@@ -0,0 +1,34 @@
+package com.example.string;
+
+import java.util.Scanner;
+
+//24.java program to start with method with using start with method
+
+public class StartWithFun {
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the string:");
+		String str=sc.nextLine();
+		System.out.print("Enter the 2nd string:");
+		String str1=sc.nextLine();
+		
+		int count=0;
+		for(int i=0; i<str1.length(); i++) {
+			
+			if(str.charAt(i)==str1.charAt(i)) {
+				
+				count++;
+				continue;
+			}else {
+				System.out.print("Not start with");
+				break;
+			}
+		}
+		if(count==str1.length()) {
+			System.out.print("start with");
+		}
+		
+		
+	}
+}
